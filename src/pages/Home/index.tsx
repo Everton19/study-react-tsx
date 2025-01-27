fa73e749f311f39fbedae89cf4e7288fb6dca7e4
Everton19 <evertontp5@gmail.com>
2025-01-27 00:03:39 -0300

    criação do projeto de estudo

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import "./style.scss"
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite seu nome.." />
+      <button>Adicionar</button>
+    </div>
+  )
+}
+
+export default Home
