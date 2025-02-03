5809be7d60277d70620002b96e33085578f5ecf8
Everton19 <evertontp5@gmail.com>
2025-02-03 00:12:24 -0300

    adicionando header e estilizando

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,20 +3,51 @@ import { Card } from "../../components/Card"
 import { useState } from "react"
 
 function Home() {
-  const [studentName, setStudentName] = useState("")
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      id: Math.random(),
+      name: studentName,
+      time: new Date().toLocaleTimeString(
+        "pt-br",
+        { hour: '2-digit', minute: '2-digit' }
+      )
+    }
+
+    setStudents(prev => 
+      [...prev, newStudent]
+    );
+  }
 
   return (
     <div className="container">
-      <h1>{studentName}</h1>
+      
+      <header>
+          <h1>Lista de presença</h1>
+        <div>
+          <strong>Everton</strong>
+          <img src="https://cdn.myanimelist.net/images/characters/15/559134.jpg"/>
+        </div>
+      </header>
+      
       <input 
         type="text" 
         placeholder="Digite seu nome.."
         onChange={e => setStudentName(e.target.value)} 
       />
-      <button>Adicionar</button>
+      <button onClick={handleAddStudent}>Adicionar</button>
 
-      <Card name="Faisca" time="10:55:12" />
-      <Card name="Everton" time="12:52:14" />
+      {
+        students.map(x => 
+          <Card
+            key={x.id}
+            name={x.name} 
+            time={x.time} 
+          />
+        )
+      }
     </div>
   )
 }
